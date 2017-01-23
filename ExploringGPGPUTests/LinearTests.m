94b20f8ad1c346d9c5cba7fce3aeb8b01cd0b696
Francis Chong <francis@ignition.hk>
2017-01-23 13:39:32 +0800

    add ios tests

--- ExploringGPGPUTests/LinearTests.m
+++ ExploringGPGPUTests/LinearTests.m
@@ -1,29 +1,28 @@
 //
-//  Created by Bartosz Ciechanowski on 23.12.2013.
-//  Copyright (c) 2013 Bartosz Ciechanowski. All rights reserved.
+//  LinearTests.m
+//  ExploringGPGPU
+//
+//  Created by Francis Chong on 23/1/2017.
+//  Copyright © 2017 Bartosz Ciechanowski. All rights reserved.
 //
 
+#import <XCTest/XCTest.h>
 #import <OpenGLES/ES3/gl.h>
 #import <OpenGLES/ES2/glext.h>
+#import <GLKit/GLKit.h>
 
-#import "ViewController.h"
-
+#import "Tester.h"
 #import "LinearTester.h"
-#import "ExponentTester.h"
 #import "IterativeTester.h"
 
-
 const int Count = 1 << 24;
-
 const int ProfileIterations = 8;
 
-
-@interface ViewController () {
-    
+@interface LinearTests : XCTestCase {
     GLuint gpuReadBuffer;
     GLuint gpuWriteBuffer;
     GLuint vao;
-    
+
     float *cpuReadBuffer;
     float *cpuWriteBuffer;
 }
@@ -33,38 +32,58 @@ const int ProfileIterations = 8;
 
 @end
 
+@implementation LinearTests
 
-@implementation ViewController
+- (void)setUp {
+    [super setUp];
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    
     self.context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES3];
-    
     if (!self.context) {
-        NSLog(@"This application requires OpenGL ES 3.0");
-        abort();
+        XCTFail(@"This application requires OpenGL ES 3.0");
     }
-    
-    
-    GLKView *view = (GLKView *)self.view;
+
+    GLKView *view = [[GLKView alloc] initWithFrame:CGRectMake(0, 0, 1024, 768)];
     view.context = self.context;
     view.drawableDepthFormat = GLKViewDrawableDepthFormat24;
-    
+
     [EAGLContext setCurrentContext:self.context];
-    
-    self.tester = [LinearTester new];
-    //self.tester = [ExponentTester new];
-    //self.tester = [IterativeTester new];
-    
+
+    self.tester = [IterativeTester new];
+
     self.tester.valuesCount = Count;
     [self.tester loadShaders];
-    
+
     [self setupCPUBuffers];
     [self setupGPUBuffers];
     [self fillBuffers];
 }
+
+- (void)tearDown {
+    [super tearDown];
+
+    glBindBuffer(GL_ARRAY_BUFFER, gpuWriteBuffer);
+    float *gpuMemoryBuffer = glMapBufferRange(GL_ARRAY_BUFFER, 0, sizeof(float) * Count, GL_MAP_READ_BIT);
+
+    [self compareCPUBuffer:cpuWriteBuffer withGPUBuffer:gpuMemoryBuffer];
+
+    glUnmapBuffer(GL_ARRAY_BUFFER);
+}
+
+- (void)testCPU {
+    [self measureBlock:^{
+        [self.tester calculateCPUWithReadBuffer:cpuReadBuffer writeBuffer:cpuWriteBuffer count:262144];
+    }];
+}
+
+- (void)testGPU {
+    // warm up gpu
+    [self.tester calculateGPUWithReadVAO:vao writeBuffer:gpuWriteBuffer count:128];
+
+    [self measureBlock:^{
+        [self.tester calculateGPUWithReadVAO:vao writeBuffer:gpuWriteBuffer count:262144];
+    }];
+}
+
 
 - (void)setupCPUBuffers
 {
@@ -82,7 +101,7 @@ const int ProfileIterations = 8;
     glBindVertexArrayOES(vao);
 
     int vectorOutputs = [self.tester vectorOutputs];
-    
+
     for (int i = 0; i < vectorOutputs; i++) {
         glEnableVertexAttribArray(i);
         glVertexAttribPointer(i, 4, GL_FLOAT, GL_FALSE, (4 * vectorOutputs) * sizeof(float), (void *)(i * 4 * sizeof(float)));
@@ -94,119 +113,61 @@ const int ProfileIterations = 8;
     glGenBuffers(1, &gpuWriteBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, gpuWriteBuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(float) * Count, NULL, GL_STREAM_DRAW);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);    
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
 - (void)fillBuffers
 {
     glBindBuffer(GL_ARRAY_BUFFER, gpuReadBuffer);
     float *gpuRead = glMapBufferRange(GL_ARRAY_BUFFER, 0, sizeof(float) * Count, GL_MAP_WRITE_BIT);
-    
+
     [self.tester fillCPUBuffer:cpuReadBuffer GPUBuffer:gpuRead];
-    
+
     glUnmapBuffer(GL_ARRAY_BUFFER);
 }
 
-- (void)profileWithBlock:(void (^)(int count))profileBlock;
-{
-    NSTimeInterval last = 0.0f;
-    for (int count = 1 << 12; count <= Count; count <<= 1) {
-        NSDate *start = [NSDate date];
-        
-        for (int iter = 0; iter < ProfileIterations; iter++) {
-            profileBlock(count);
-        }
-        
-        last = [[NSDate date] timeIntervalSinceDate:start]/ProfileIterations;
-        printf("%g\n", last);
-    }
-}
-
-
-- (void)measureCPU
-{
-    printf("CPU\n");
-
-    [self profileWithBlock:^(int count) {
-        [self.tester calculateCPUWithReadBuffer:cpuReadBuffer writeBuffer:cpuWriteBuffer count:count];
-    }];
-}
-
-- (void)measureGPU
-{
-    // shader warmup
-    [self.tester calculateGPUWithReadVAO:vao writeBuffer:gpuWriteBuffer count:128];
-    
-    printf("GPU\n");
-    
-    [self profileWithBlock:^(int count) {
-        [self.tester calculateGPUWithReadVAO:vao writeBuffer:gpuWriteBuffer count:count];
-    }];
-}
-
-- (void)measure
-{
-    [self measureCPU];
-    [self measureGPU];
-}
-
-- (void)viewDidAppear:(BOOL)animated
-{
-    [self measure];
-    
-    glBindBuffer(GL_ARRAY_BUFFER, gpuWriteBuffer);
-    float *gpuMemoryBuffer = glMapBufferRange(GL_ARRAY_BUFFER, 0, sizeof(float) * Count, GL_MAP_READ_BIT);
-    
-    [self compareCPUBuffer:cpuWriteBuffer withGPUBuffer:gpuMemoryBuffer];
-    
-    glUnmapBuffer(GL_ARRAY_BUFFER);
-}
-
-// based on
-// http://randomascii.wordpress.com/2012/02/25/comparing-floating-point-numbers-2012-edition/
-//
 - (void)compareCPUBuffer:(float *)cpuBuffer withGPUBuffer:(float *)gpuBuffer
 {
     union Float_t {
         float f;
         int32_t i;
     };
-    
+
     BOOL isCalculationCorrect = YES;
-    
+
     float errorSum = 0.0f;
     float maxError = 0.0f;
     float minError = MAXFLOAT;
-    
+
     int32_t ULPsum = 0;
     int32_t maxULP = 0;
     int32_t minULP = INT32_MAX;
-    
+
     union Float_t cpu, gpu;
-    
+
     for (int i = 0; i < Count; i++) {
-        
+
         cpu.f = cpuBuffer[i];
         gpu.f = gpuBuffer[i];
-        
+
         float error = fabsf((cpu.f - gpu.f)/cpu.f);
-        
+
         maxError = MAX(maxError, error);
         minError = MIN(minError, error);
-        
+
         errorSum += error;
 
-        
+
         if ((cpu.i >> 31 != 0) != (gpu.i >> 31 != 0) ) {
             if (cpu.f != gpu.f) {
                 isCalculationCorrect = NO; // different sings, can't compare, sorry
             }
         }
-        
+
         int32_t ulpError = abs(cpu.i - gpu.i);
         maxULP = MAX(maxULP, ulpError);
         minULP = MIN(minULP, ulpError);
-        
+
         ULPsum += ulpError;
     }
 
@@ -215,9 +176,8 @@ const int ProfileIterations = 8;
     } else {
         printf("WARNING: the ULP error evaluation failed due to sign mismatch\n");
     }
-    
+
     printf("FLT: avg:%g max: %g, min: %g\n", errorSum/Count, maxError, minError);
 }
-
 
 @end
